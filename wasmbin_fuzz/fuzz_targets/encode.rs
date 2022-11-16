1529657ccafb8d8faaeedeffcc86be6516035997
dafifynn <slimbieber@gmail.com>
2022-11-16 16:31:52 -0300

    update comment in target encode

--- wasmbin_fuzz/fuzz_targets/encode.rs
+++ wasmbin_fuzz/fuzz_targets/encode.rs
@@ -1,5 +1,5 @@
 #![no_main]
-//to run this fuzzer you need the wasmbin repository, add this folder as fuzz in the repository and run the fuzzer with cargo fuzz
+//to run this fuzzer you need the wasmbin repository, add this folder as fuzz in the repository and run the fuzzer with cargo fuzz and if you want you can add a corpus from https://github.com/WAVM/wavm-fuzz-corpora
 use libfuzzer_sys::fuzz_target;
 
 use wasmbin::Module;
