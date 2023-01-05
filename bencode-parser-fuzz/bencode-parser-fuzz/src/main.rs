f9c2ba36818699aa231bd6e25f8fbde88f6af32c
dafifynn <slimbieber@gmail.com>
2023-01-05 10:37:46 -0500

    correct fuzzer

--- bencode-parser-fuzz/bencode-parser-fuzz/src/main.rs
+++ bencode-parser-fuzz/bencode-parser-fuzz/src/main.rs
@@ -2,6 +2,7 @@
 extern crate honggfuzz;
 use nom_bencode::Value;
 use bencoder::bencode::{Bencode, BencodeError};
+use std::collections::{BTreeMap, HashMap};
 
 
 fn main() {
@@ -10,7 +11,7 @@ fn main() {
         fuzz!(|data: &[u8]| {
             let nom_result = parse_source_nom(data).unwrap();
             let libtorrent_result = parse_source_libtorrent(data).unwrap();
-
+            
             assert_eq!(to_bencode(nom_result), libtorrent_result);
         });
     }
@@ -25,10 +26,61 @@ fn parse_source_libtorrent(src: &[u8]) -> Result<Bencode, BencodeError> {
 }
 
 fn to_bencode(value: Vec<Value>) -> Bencode {
-    match value {
-        Value::Integer => Bencode::BNumber(value),
-        Value::Bytes => Bencode::BString(value),
-        Value::List => Bencode::BList(value),
-        Value::Dictionary => Bencode::BDict(value),
+    
+    match &value[..] {
+        [Value::Integer(value), _] => Bencode::BNumber(*value),
+        [Value::Bytes(value),_] => to_string_bencode(value),
+        [Value::List(value), _] => to_list_bencode(value),
+        [Value::Dictionary(value), _] => to_dict_bencode(value),
+        [] | [_] | [_, _, _, ..] => to_bencode(value),
     }
+}
+
+fn value_to_bencode(value: &Value) -> Bencode {
+    
+    match value {
+        Value::Integer(value) => Bencode::BNumber(*value),
+        Value::Bytes(value) => to_string_bencode(value),
+        Value::List(value) => to_list_bencode(value),
+        Value::Dictionary(value) => to_dict_bencode(value),
+    }
+}
+
+fn to_string_bencode(value: &[u8]) -> Bencode {
+    let vector = from_reference_to_vec(value);
+
+    Bencode::BString(vector)
+}
+
+fn from_reference_to_vec(value: &[u8]) -> Vec<u8> {
+    let vector: Vec<u8> = value.iter().cloned().collect();
+
+    vector.to_vec()
+}
+
+fn to_list_bencode(value: &Vec<Value>) -> Bencode {
+    let bencode_vec = [];
+
+    for x in value {
+        let bencode = value_to_bencode(x);
+        bencode_vec.to_vec().push(bencode);
+    }
+
+    Bencode::BList(bencode_vec.to_vec())
+}
+
+fn to_dict_bencode(value: &HashMap<&[u8], Value>) -> Bencode {
+    let btreemap = hashmap_to_btreemap(value);
+
+    Bencode::BDict(btreemap)
+}
+
+fn hashmap_to_btreemap(hashmap: &HashMap<&[u8], Value>) -> BTreeMap<Vec<u8>, Bencode> {
+    let mut btreemap: BTreeMap<Vec<u8>, Bencode> = BTreeMap::new();
+
+    btreemap = hashmap
+        .into_iter()
+        .map(|(key, value)| (from_reference_to_vec(key), value_to_bencode(value)))
+        .collect();
+    btreemap
 }
