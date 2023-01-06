f29383c7769d3127b533eaa6d0d908d288ec1540
dafifynn <slimbieber@gmail.com>
2023-01-06 12:02:27 -0500

    fix criterion bench

--- bencode-parser-fuzz/benches/bencode_bench.rs
+++ bencode-parser-fuzz/benches/bencode_bench.rs
@@ -6,33 +6,29 @@ use nom_bencode::Value;
 use bencoder::bencode::{Bencode, BencodeError};
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use std::{fs, io};
-use rand::Rng;
 
-
-// fn parse_source(src: &[u8]) -> Result<Vec<nom_bencode::Value>, nom_bencode::Error<&[u8]>> {
-//     nom_bencode::parse(src)
-// }
+static SOURCE_BYTES: &'static [u8] = include_bytes!("../../torrents/file1.torrent");
 
 fn bencode_benchmark(c: &mut Criterion) {
-    let files = fs::read_dir("bencode-parser-fuzz/bencode-parser-fuzz/hfuzz_workspace/bencode-parser-fuzz/input/").unwrap();
 
+    // commented for following implementation
     // let paths: Vec<_> = WalkDir::new("bencode-parser-fuzz/bencode-parser-fuzz/hfuzz_workspace/bencode-parser-fuzz/input/")
     //                     .into_iter()
     //                     .filter_map(|f| f.ok()) 
     //                     .map(|f| f.path().to_str().unwrap())
     //                     .collect();
 
-    let mut paths: Vec<&str> = [].to_vec();
+    // let mut paths: Vec<&str> = [].to_vec();
 
-    for entry in files {
-        let path = entry.unwrap().path();
-        // Get path string.
-        let path_str = path.to_str().unwrap();
-        paths.push(&path_str);
-    }
+    // for entry in files {
+    //     let path = entry.unwrap().path();
+    //     // Get path string.
+    //     let path_str = path.to_str().unwrap();
+    //     paths.push(&path_str);
+    // }
     
-    c.bench_function("bencode torrent nom", |b| b.iter(|| parse_source_nom(black_box(paths[0].as_bytes()))));
-    c.bench_function("bencode torrent libtorrent", |b| b.iter(|| parse_source_libtorrent(black_box(paths[0].as_bytes()))));
+    c.bench_function("bencode torrent nom", |b| b.iter(|| parse_source_nom(black_box(SOURCE_BYTES))));
+    c.bench_function("bencode torrent libtorrent", |b| b.iter(|| parse_source_libtorrent(black_box(SOURCE_BYTES))));
 }
 
 criterion_group!(benches, bencode_benchmark);
