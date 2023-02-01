3b210c8956f01b3064a1e99efe4cfda0f8569ff2
dafifynn <slimbieber@gmail.com>
2023-02-01 16:23:03 -0300

    add mutator to test more than one insert in fuzzer

--- merkle_patricia_tree/merkle_tree/src/main.rs
+++ merkle_patricia_tree/merkle_tree/src/main.rs
@@ -7,7 +7,8 @@ use patricia_merkle_tree::PatriciaMerkleTree;
 use sha3::Keccak256;
 use proptest::prelude::*;
 use std::sync::Arc;
-use crate::prop::collection::vec;
+
+const MAX_INPUT_SIZE: usize = 1024;
 
 fn main() {
     loop {
@@ -18,33 +19,35 @@ fn main() {
             println!("{:?}", data);
 
             let mut tree = PatriciaMerkleTree::<&[u8], &[u8], Keccak256>::new();
-            
-            let root_hash = tree.compute_hash().as_slice().to_vec();
         
             let memdb = Arc::new(MemoryDB::new(true));
             let hasher = Arc::new(HasherKeccak::new());
             let mut trie = PatriciaTrie::new(Arc::clone(&memdb), Arc::clone(&hasher));
+            let mut init_input: Vec<u8> = data.to_vec();
+            let inputs = mutate(&mut init_input);
 
-            let inputs = mutate(data);
 
+            for input in inputs.iter() {
+                trie.insert(input.to_vec(), input.to_vec());
 
-            for input in inputs {
-                tree.insert(input, input);
-                trie.insert(input.to_vec(), input.to_vec()).unwrap();
+                tree.insert(&input, &input);
+
+                let root_hash = tree.compute_hash().as_slice().to_vec();
+                let reference_root = trie.root().unwrap();
+                println!("{:?}", reference_root);
+                println!("{:?}", root_hash);
+            
+                assert_eq!(
+                reference_root,
+                root_hash
+                )
             };
             
-            let reference_root = trie.root().unwrap();
-            println!("{:?}", reference_root);
-            println!("{:?}", root_hash);
-        
-            assert_eq!(
-            reference_root,
-            root_hash
-            )
         });
     }
 }
 
+#[derive(Copy, Clone)]
 struct Rng {
     /// The RNG's seed and state
     seed: u64,
@@ -107,9 +110,9 @@ impl Rng {
 }
 
 
-fn mutate(input: &[u8]) -> Vec<&[u8]> {
+fn mutate(input:  &mut [u8]) -> Vec<Vec<u8>> {
     /// List of mutation strategies which do not require an input database
-    const STRATEGIES: &[fn()] = &[
+    const STRATEGIES: &[for<'a> fn(&'a mut [u8], Rng) -> Vec<u8>; 9] = &[
         shrink,
         expand,
         add_sub,
@@ -117,8 +120,8 @@ fn mutate(input: &[u8]) -> Vec<&[u8]> {
         swap,
         copy,
         inter_splice,
-        // Mutator::insert_rand,
-        // Mutator::overwrite_rand,
+        insert_rand,
+        overwrite_rand,
         // Mutator::byte_repeat_overwrite,
         // Mutator::byte_repeat_insert,
         // Mutator::magic_overwrite,
@@ -131,16 +134,16 @@ fn mutate(input: &[u8]) -> Vec<&[u8]> {
         // Mutator::neg_byte,
     ];
 
-    let mut vec = vec![input];
+    let mut vec = vec![];
 
-    let rng = Rng {
+    let mut rng = Rng {
         seed:         0x12640367f4b7ea35,
         exp_disabled: false,
     };
 
     for _ in 0..5 {
         // Pick a random mutation strategy
-        let sel = rng.rand(0, STRATEGIES.len() - 1);
+        let sel = rng.rand(0, STRATEGIES.len() - 1).clone();
 
         // Save the old state of the exponential random and randomly disable
         // the exponential random
@@ -154,17 +157,20 @@ fn mutate(input: &[u8]) -> Vec<&[u8]> {
         let strat = STRATEGIES[sel];
 
         // Run the mutation strategy
-        vec.push(strat(input, rng));
+        let new_input = strat(input, rng);
+
+        // Add new mutation to vec
+        vec.push(new_input);
 
         // Restore exponential random state to the old state
         rng.exp_disabled = old_exp_state;
         
     };
-
+     
     vec
 }
 
-fn rand_exp(rnd: Rng, min: usize, max: usize) -> usize {
+fn rand_exp(mut rnd: Rng, min: usize, max: usize) -> usize {
 
     if rnd.rand(0, 1) == 0 {
         // Half the time, provide uniform
@@ -176,7 +182,7 @@ fn rand_exp(rnd: Rng, min: usize, max: usize) -> usize {
     }
 }
 
-fn rand_offset(rnd: Rng,input: &[u8]) -> usize {
+fn rand_offset(rnd: Rng, input: &[u8]) -> usize {
     if !input.is_empty() {
         // just return a random index
         rand_exp(rnd, 0, input.len() - 1 as usize)
@@ -189,10 +195,10 @@ fn rand_offset(rnd: Rng,input: &[u8]) -> usize {
 }
 
 /// Randomly delete a chunk of the input
-fn shrink(input: &[u8], rng: Rng) {
+fn shrink(input:  &mut [u8], mut rng: Rng) -> Vec<u8> {
     // Nothing to do on an empty input
     if input.is_empty() {
-        return;
+        return input.to_vec();
     }
 
     // Pick a random offset to remove data at
@@ -216,10 +222,13 @@ fn shrink(input: &[u8], rng: Rng) {
     let to_remove = rng.rand_exp(1, max_remove);
 
     // Remove the bytes from the input
-    let _ = input.drain(offset..offset + to_remove);
+    let mut binding = input.to_vec();
+    let new_input = binding.drain(offset..offset + to_remove).as_slice().to_vec();
+
+    new_input
 }
 
-fn expand(input: &[u8], rng: Rng) {
+fn expand(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
     
     // Pick a random offset to expand at
     let offset = rand_offset(rng, input);
@@ -240,15 +249,17 @@ fn expand(input: &[u8], rng: Rng) {
     let iter = core::iter::repeat(b'\0').take(rng.rand_exp(1, max_expand)); 
     
     // Expand at `offset` with `iter`
-    input.splice(offset..offset, iter)
+    input.to_vec().splice(offset..offset, iter);
+
+    input.to_vec()
 }
 
 /// Add or subtract a random amount with a random endianness from a random
 /// size `u8` through `u64`
-fn add_sub(input: &[u8], rng: Rng) {
+fn add_sub(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
     // Nothing to do on an empty input
     if input.is_empty() {
-        return;
+        return input.to_vec();
     }
 
     // Pick an offset to corrupt at
@@ -296,7 +307,8 @@ fn add_sub(input: &[u8], rng: Rng) {
 
             // Write the new value out to the input
             input[offset..offset + intsize].copy_from_slice(
-                &tmp.to_ne_bytes())
+                &tmp.to_ne_bytes());
+            input.to_vec()
         }}
     }
 
@@ -307,16 +319,16 @@ fn add_sub(input: &[u8], rng: Rng) {
         4 => mutate!(u32),
         8 => mutate!(u64),
         _ => unreachable!(),
-    };
+    }
 
 }
 
 /// Randomly replace a sequence of bytes with the same random character
 /// repeated a random amount of times
-fn set(input: &[u8], rng: Rng) {
+fn set(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
     // Nothing to do on an empty input
     if input.is_empty() {
-        return;
+        return input.to_vec();
     }
 
     // Pick offset to memset at
@@ -330,13 +342,15 @@ fn set(input: &[u8], rng: Rng) {
 
     // Replace the selected bytes at the offset with `chr`
     input[offset..offset + len].iter_mut().for_each(|x| *x = chr);
+
+    input.to_vec()
 }
 
 /// Swap two difference sequence of bytes in the input to different places
-fn swap(input: &[u8], rng: Rng) {
+fn swap(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
     // Nothing to do on an empty input
     if input.is_empty() {
-        return;
+        return input.to_vec();
     }
 
     // Pick two random ranges in the input and calculate the remaining
@@ -350,12 +364,12 @@ fn swap(input: &[u8], rng: Rng) {
     let len = rng.rand_exp(1, core::cmp::min(srcrem, dstrem));
 
     // Swap the ranges of bytes
-    swap_ranges(input, src, dst, len);
+    swap_ranges(input, src, dst, len)
 }
 
 /// Swap two ranges in an input buffer
-fn swap_ranges(input: &[u8], mut offset1: usize, mut offset2: usize,
-    mut len: usize) {
+fn swap_ranges(input: &mut [u8], mut offset1: usize, mut offset2: usize,
+    mut len: usize) -> Vec<u8> {
     if offset1 < offset2 && offset1 + len >= offset2 {
     // The ranges have the following layout here:
     // [o1--------]
@@ -368,7 +382,7 @@ fn swap_ranges(input: &[u8], mut offset1: usize, mut offset2: usize,
     // values at offset1 are not mangled in the process of copying.
     // Same as memmove.
     for ii in (tail..len).rev() {
-        vec[offset2 + ii] = vec[offset1 + ii];
+        input[offset2 + ii] = input[offset1 + ii];
     }
 
     // After this, the layout is the following:
@@ -384,7 +398,7 @@ fn swap_ranges(input: &[u8], mut offset1: usize, mut offset2: usize,
     //      [head1][o1-]
     // [head2][o2-]
     for ii in 0..head {
-        vec[offset2 + ii] = vec[offset1 + ii];
+        input[offset2 + ii] = input[offset1 + ii];
     }
 
     // After this, the layout is the following:
@@ -400,14 +414,16 @@ fn swap_ranges(input: &[u8], mut offset1: usize, mut offset2: usize,
     for ii in 0..len {
     input.swap(offset1 + ii, offset2 + ii);
     }
+
+    input.to_vec()
 }
 
 /// Copy bytes from one location in the input and overwrite them at another
 /// location in the input
-fn copy(input: &[u8], rng: Rng) {
+fn copy(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
     // Nothing to do on an empty input
     if input.is_empty() {
-        return;
+        return input.to_vec();
     }
 
     // Pick a source and destination for a copy
@@ -420,33 +436,37 @@ fn copy(input: &[u8], rng: Rng) {
     let len = rng.rand_exp(1, core::cmp::min(srcrem, dstrem));
 
     // Perform a copy inplace in the input
-    overwrite_inplace(input, src, len, dst);
+    overwrite_inplace(input, src, len, dst)
 }
 
 /// Take the bytes from `source` for `len` bytes in the input, and copy
 /// them to `dest`
-fn overwrite_inplace(input: &[u8], source: usize, len: usize, dest: usize) {
+fn overwrite_inplace(input: &mut [u8], source: usize, len: usize, dest: usize) -> Vec<u8> {
     // Nothing to do
-    if len == 0 || source == dest { return; }
+    if len == 0 || source == dest { return input.to_vec(); }
 
     if source < dest {
         // Copy forwards
         for ii in 0..len {
             input[dest + ii] = input[source + ii];
         }
+
+        input.to_vec()
     } else {
         // Copy backwards
         for ii in (0..len).rev() {
             input[dest + ii] = input[source + ii];
         }
+
+        input.to_vec()
     }
 }
 
 /// Take one location of the input and splice it into another
-fn inter_splice(input: &[u8], rng: Rng) {
+fn inter_splice(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
     // Nothing to do on an empty input
     if input.is_empty() {
-        return;
+        return input.to_vec();
     }
 
     // Pick a source and destination for an insertion
@@ -458,34 +478,102 @@ fn inter_splice(input: &[u8], rng: Rng) {
     let len = rng.rand_exp(1, srcrem);
 
     // Perform an insertion inplace in the input
-    insert_inplace(input, src, len, dst);
+    insert_inplace(input, src, len, dst)
 }
 
 /// Take the bytes from `source` for `len` bytes in the input, and insert
 /// a copy of them at `dest`
-fn insert_inplace(input: &[u8], source: usize, len: usize, dest: usize) {
+fn insert_inplace(input: &mut [u8], source: usize, len: usize, dest: usize)-> Vec<u8> {
     // Nothing to do
-    if len == 0 || source == dest { return; }
+    if len == 0 || source == dest { return input.to_vec(); }
 
     // Cap the insertion to the max input size
-    let len = core::cmp::min(len, 128 - input.len());
+    let len = core::cmp::min(len, MAX_INPUT_SIZE - input.len());
 
     // Create an interator to splice into the input
     let rep = core::iter::repeat(b'\0').take(len);
 
     // Expand at `dest` with `rep`, making room for the copy
-    input.splice(dest..dest, rep);
+    let mut input_vec = input.to_vec();
+    input_vec.splice(dest..dest, rep);
 
     // Determine where the splice occurred
     let split_point = dest.saturating_sub(source).min(len);
 
     for ii in 0..split_point {
-        input[dest + ii] = input[source + ii];
+        input_vec[dest + ii] = input_vec[source + ii];
     }
     
     for ii in split_point..len {
-        input[dest + ii] = input[source + ii + len];
+        input_vec[dest + ii] = input_vec[source + ii + len];
     }
+
+    let new_input = &input_vec;
+
+    new_input.to_vec()
 }
 
+/// Create 1 or 2 random bytes and insert them into the input
+fn insert_rand(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
+    // Pick some random values
+    let bytes = [rng.rand(0, 255) as u8, rng.rand(0, 255) as u8];
 
+
+    // Pick a random offset and length
+    let offset = rand_offset(rng, input);
+    let len = rng.rand(1, 2);
+
+    // Insert the bytes
+    insert(input, offset, &bytes[..len])
+}
+
+/// Insert `buf` at `offset` in the input. `buf` will be truncated to
+/// ensure the input stays within the maximum input size
+fn insert<'a>(input: &'a[u8], offset: usize, buf: &[u8]) -> Vec<u8> {
+    // Make sure we don't expand past the maximum input size
+    let len =
+        core::cmp::min(buf.len(), MAX_INPUT_SIZE - input.len());
+
+    // Splice in the `buf`
+    input.to_vec().splice(offset..offset, buf[..len].iter().copied());
+
+    input.to_vec()
+    
+}
+
+/// Create 1 or 2 random bytes and overwrite them at a location in the
+/// input
+fn overwrite_rand(input: &mut [u8], mut rng: Rng) -> Vec<u8> {
+    // Nothing to do on an empty input
+    if input.is_empty() {
+        return input.to_vec();
+    }
+
+    // Pick some random values
+    let bytes = [rng.rand(0, 255) as u8, rng.rand(0, 255) as u8];
+
+    // Pick a random offset and length
+    let offset = rand_offset(rng, input);
+    let len = core::cmp::min(input.len() - offset, 2);
+    let len = rng.rand(1, len);
+
+    // Overwrite the bytes
+    let new_input = input;
+    overwrite(new_input, offset, &bytes[..len])
+}
+
+/// Overwrite the bytes in the input with `buf` at `offset`. If `buf`
+/// goes out of bounds of the input the `buf` will be truncated and the
+/// copy will stop.
+fn overwrite<'a>(input: &'a mut [u8], offset: usize, buf: &[u8]) -> Vec<u8> {
+    // Get the slice that we may overwrite
+    let target = &mut input[offset..];
+
+    // Get the length to overwrite
+    let len = core::cmp::min(buf.len(), target.len());
+
+    // Overwrite the bytes
+    target[..len].copy_from_slice(&buf[..len]);
+
+    target.to_vec()
+}
